c02e013b419d286ac1def7b5d2dcf7e3ffee0de9
ypark79 <paul.park86@gmail.com>
2026-01-23 17:40:46 -1000

    Updated code in run.py, pages.py, and main.py to match the instructor's example in the module 1 lecture slides.

--- module_1/pages/pages.py
+++ module_1/pages/pages.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, render_template
 
-pages_bp = Blueprint('pages', __name__)
+bp = Blueprint('pages', __name__, template_folder='templates')
 
-@pages_bp.route('/')
+@bp.route('/')
 def home():
     return render_template('home.html')
 
-@pages_bp.route('/contact')
+@bp.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@pages_bp.route('/projects')
+@bp.route('/projects')
 def projects():
     return render_template('projects.html')
