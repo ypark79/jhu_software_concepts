df82bf6085d0195a87cfe24aa021c8e755c91423
ypark79 <paul.park86@gmail.com>
2026-01-25 14:46:57 -1000

    Uploaded a screenshot of the robots.txt to verify I am legally allowed to scrape data from the grad cafe website. Started scrape.py to build hte code to make the web connection using urllib3, retrieve the html, and parse through the data suing a BeautifulSoup object.

---
+++ module_2/scrape.py
@@ -0,0 +1,32 @@
+import urllib3
+from bs4 import BeautifulSoup
+
+http = urllib3.PoolManager()
+
+url = "https://www.thegradcafe.com/survey/"
+
+http_response = http.request('GET', url)
+
+html_conversion = http_response.data.decode('utf-8')
+
+soup = BeautifulSoup(html_conversion, 'html.parser')
+
+rows = soup.find_all('tr')
+data_rows = []
+for r in rows:
+    if r.find('td') is not None:
+        data_rows.append(r)
+
+singe_row = data_rows[0]
+cells = singe_row.find_all('td')
+
+print(cells)
+
+
+
+
+
+
+
+
+
